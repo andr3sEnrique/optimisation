aec16147bd3956b016d15e8811f0f1a12618ced1
andr3sEnrique <20213tn080@utez.edu.mx>
2023-09-28 15:44:53 +0200

    liste fini

---
+++ Téléchargements/django-master/django-master/td2/main.c
@@ -0,0 +1,15 @@
+#include "liste.h"
+#include <stdio.h>
+
+int main(){
+    lst liste = creerListe();
+    printf("%d \n", liste->nbElem);
+    ajouterEnTete(liste, 3);
+    ajouterEnTete(liste, 6);
+    ajouterEnTete(liste, 7);
+    ajouterEnTete(liste, 98);
+    afficherListe(liste);
+    supprimerEnTete(liste);
+    afficherListe(liste);
+    return 0;
+}
